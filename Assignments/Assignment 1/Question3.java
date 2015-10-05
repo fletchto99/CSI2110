283e9c54c8a950d7fc057b2f0ce6d0dd83817957
Matt Langlois <fletchto99@gmail.com>
2015-10-05 14:34:54 -0400

    Question 3 of assignment 1

--- Assignments/Assignment 1/Question3.java
+++ Assignments/Assignment 1/Question3.java
@@ -1,5 +1,20 @@
-/**
- * Created by mattlanglois on 15-10-03.
- */
 public class Question3 {
+
+    public static int swaps = 0;
+
+
+    public static void main(String[] args) {
+
+        perm(3, 0);
+        System.out.println(swaps);
+    }
+
+    static void perm(int n, int i) {
+        if (i != n) {
+            for (int j = i; j < n; j++) {
+                swaps += 2;
+                perm(n, i + 1);
+            }
+        }
+    }
 }
