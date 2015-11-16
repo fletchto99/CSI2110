2bab92b010aa15b71caec8862f3df9480708ee96
Matt Langlois <fletchto99@gmail.com>
2015-11-16 13:43:02 -0500

    Finish up lab 3 with proper methods

--- Assignments/Assignment 3/DecisionTree.java
+++ Assignments/Assignment 3/DecisionTree.java
@@ -22,38 +22,48 @@ public class DecisionTree {
     // replace a leaf node by a DecisionStump with two leaves
     public void replace(DecisionStump leaf, int featureIndex, double threshold) {
         if (!leaf.isExternal()) {
-            throw new RuntimeException("Leaf must be external to be replaced!");
+            throw new RuntimeException("Leaf being replaced must be external!");
         }
+
         DecisionStump replacement = new DecisionStump(featureIndex, threshold);
         replacement.setGreaterBranch(new DecisionStump());
         replacement.setSmallerBranch(new DecisionStump());
-        DecisionStump parent;
-        if ((parent = leaf.getParent()) != null) {
+
+        DecisionStump parent = leaf.getParent();
+        if (parent != null) {
             if (parent.getSmallerBranch() == leaf) {
                 parent.setSmallerBranch(replacement);
             } else {
                 parent.setGreaterBranch(replacement);
             }
         } else {
-            root = replacement;
+            this.root = replacement;
         }
 
     }
 
     // gets the leaf with the smallest maximal class probability
     public DecisionStump getSmallestMaxProb() {
-        return getSmallestMaxProbRecursive(this.root);
+        DecisionStump smallest = getSmallestMaxProbRecursive(this.root, null);
+        if (!smallest.isExternal()) {
+            System.out.println("Uh oh how did we get here??");
+        }
+        return smallest;
     }
 
-    private DecisionStump getSmallestMaxProbRecursive(DecisionStump smallest) {
-        return smallest.getSmallerBranch() != null ? smallest.getSmallerBranch() : smallest;
+    private DecisionStump getSmallestMaxProbRecursive(DecisionStump current, DecisionStump smallest) {
+        if(!current.isExternal()) {
+            DecisionStump left = getSmallestMaxProbRecursive(current.getSmallerBranch(), smallest);
+            DecisionStump right = getSmallestMaxProbRecursive(current.getGreaterBranch(), smallest);
+            return left.getMaxProb() < right.getMaxProb() ? left : right;
+        }
+        return (smallest == null || current.getMaxProb() < smallest.getMaxProb()) ? current : smallest;
     }
 
 
     // updates the probability count of each node of the decision tree
     // based on a sample for which the associated class is known
     public void train(double[] vector, int classNumber) {
-
         root.updateProbCount(vector, classNumber);
     }
 
@@ -79,12 +89,12 @@ public class DecisionTree {
     }
 
     private void preOrderPrint(DecisionStump stump) {
-        System.out.println(stump);
+        System.out.printf("%s max probability class is %d with %.2f%%%n", stump, stump.getMaxClass(), (stump.getMaxProb() * 100));
         DecisionStump left, right;
         if ((left = stump.getSmallerBranch()) != null) {
             preOrderPrint(left);
         }
-        if ((right = stump.getSmallerBranch()) != null) {
+        if ((right = stump.getGreaterBranch()) != null) {
             preOrderPrint(right);
         }
     }
--- Assignments/Assignment 3/DecisionTreeTest.java
+++ Assignments/Assignment 3/DecisionTreeTest.java
@@ -2,39 +2,98 @@ import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.List;
+import java.util.Random;
+import java.util.stream.IntStream;
 
 public class DecisionTreeTest {
 
+    private static final boolean MODE_RANDOM = true;
+
     public static void main(String[] args) {
-        // here is how to read the file containing the pre-classified samples
-        List<String> lines = null;
-        try {
-            lines = Files.readAllLines(Paths.get("/Users/mattlanglois/Documents/Programming/School/CSI2110/Assignments/Assignment 3/iris.data.txt"), StandardCharsets.UTF_8);
-        } catch (Exception e) {
-            System.out.println("Error reading file");
+        if (args.length != 1) {
+            System.err.println("Please specify a path to the file!");
+            System.exit(1);
         }
 
-        if (lines != null) {
-            DecisionTree dt = new DecisionTree(6,3);
-            dt.replace(dt.getRoot(), 0, 5);
-            train(lines, dt);
+        try {
+            final List<String> lines = Files.readAllLines(Paths.get(args[0]), StandardCharsets.UTF_8);
+
+            if (lines != null) {
+
+                if (!MODE_RANDOM) {
+                    System.out.println("Creating decision tree");
+                    DecisionTree dt = new DecisionTree(6,3);
+                    System.out.println();
+
+                    System.out.println("Updating root to use index=0 threshold=5");
+                    dt.replace(dt.getRoot(), 0, 5);
+                    System.out.println();
+
+                    System.out.println("Training decision tree from file specified");
+                    train(lines, dt);
+                    System.out.println();
+
+                    System.out.println("Printing the nodes of the decision tree");
+                    dt.print();
+                    System.out.println();
+
+                    System.out.println("Replacing node with smallest maximal probability");
+                    dt.replace(dt.getSmallestMaxProb(), 2, 2.5);
+
+                    System.out.println("Resetting decision tree");
+                    dt.resetAll();
+                    System.out.println();
+
+                    System.out.println("Retraining decision tree");
+                    train(lines, dt);
+                    System.out.println();
+
+                    System.out.println("Printing decision tree");
+                    dt.print();
+                    System.out.println();
+
+                    System.out.println("Replacing smallest max prob node");
+                    dt.replace(dt.getSmallestMaxProb(), 1, 3);
+                    System.out.println();
 
 
-            System.out.println("Printing first time");
-            dt.print();
+                    System.out.println("Resetting decision tree");
+                    dt.resetAll();
+                    System.out.println();
 
-            System.out.println("Printing second time");
-            System.out.println(dt.getSmallestMaxProb());
+                    System.out.println("Retraining decision tree");
+                    train(lines, dt);
+                } else {
 
-            System.out.println("printing 3rd time");
-            dt.replace(dt.getSmallestMaxProb(), 2, 2.5);
-            dt.resetAll();
-            train(lines, dt);
-            dt.print();
+                    final Random r = new Random();
 
-            dt.replace(dt.getSmallestMaxProb(), 1, 3);
-            dt.resetAll();
-            train(lines, dt);
+                    //Generate random threshold and indexes
+
+                    //Build the decision tree
+                    final DecisionTree dt = new DecisionTree(6, 3);
+                    dt.replace(dt.getRoot(), r.nextInt(5),  r.nextDouble() * 10.0);
+                    train(lines, dt);
+
+                    int iterations = r.nextInt(50);
+
+                    IntStream.rangeClosed(0 , iterations).forEach(i->{
+                        //replace the smallest node
+                        dt.replace(dt.getSmallestMaxProb(), r.nextInt(5), r.nextDouble() * 10.0);
+
+                        //Reset the tree
+                        dt.resetAll();
+
+                        //Train the tree with the lines we read
+                        train(lines, dt);
+
+                    });
+
+                    dt.print();
+                }
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+            System.out.println("Error reading file");
         }
     }
 
@@ -57,7 +116,7 @@ public class DecisionTreeTest {
                 throw new RuntimeException("Invalid file configuration");
             }
             double[] path = new double[4];
-            for(int i = 0; i < split.length; i++) {
+            for(int i = 0; i < 5; i++) {
                 if (i < 4) {
                     path[i] = Double.parseDouble(split[i]);
                 } else {
